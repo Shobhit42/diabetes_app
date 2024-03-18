1a8c0753d5af1f7ec70bd57736961798ab20bfae
Shobhit Singh <singh.shobhit4225@gmail.com>
2024-03-18 18:26:18 +0530

    deploy backend of diabetes - 2

--- app.py
+++ app.py
@@ -103,5 +103,5 @@ def predict():
     return jsonify({'diabetes': str(result)})
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
